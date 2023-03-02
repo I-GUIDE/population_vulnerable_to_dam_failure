91ff7ba22abb27e4caf41dd37b2741c8f363f3bc
jparkgeo <jparkgeo@gmail.com>
2023-03-02 18:17:51 -0500

    update sbatch

--- run.sh
+++ run.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
+# for i in {0..14} 
+# do
+#     sbatch h4_sbatch.sbatch $i
+#     echo $i "Regular run"
+# done
+
 for i in {0..14} 
 do
-    sbatch h4_sbatch.sbatch $i
-    echo $i "Regular run"
+    if [ $i -lt 11 ] 
+    then 
+        sbatch h4_sbatch.sbatch $i
+        echo $i "Regular run"
+    else
+        sbatch h12_sbatch.sbatch $i
+        echo $i "Intense run"
+    fi
 done
-
-# for i in {0..16} 
-# do
-#     if [ $i -lt 17 ] 
-#     then 
-#         sbatch h4_sbatch.sbatch $i
-#         echo $i "Regular run"
-#     else
-#         sbatch h24_sbatch.sbatch $i
-#         echo $i "Intense run"
-#     fi
-# done
